e92d2a2a77b0b7c22d351d679c61ffd29dba4433
Antonyxit <carlosmorga45@gmail.com>
2025-01-21 21:34:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ const connectionOptions = {
     logger: Pino({ level: 'silent' }),
     printQRInTerminal: opcion === '1' || methodCodeQR,
     mobile: MethodMobile,
-    browser: opcion === '1' ? ['𝙃𝘼𝙍𝙇𝙀𝙔 𝘽𝙊𝙏', 'Safari', '2.0.0'] : methodCodeQR ? ['𝙃𝘼𝙍𝙇𝙀𝙔 𝘽𝙊𝙏', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
+    browser: opcion === '1' ? ['TUMBADO 𝘽𝙊𝙏', 'Safari', '2.0.0'] : methodCodeQR ? ['TUMBADO 𝘽𝙊𝙏', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '20.0.04'],
     auth: {
         creds: state.creds,
         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: 'fatal' }).child({ level: 'fatal' })),
