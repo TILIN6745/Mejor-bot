c73ea3dee29432d98e460e8a9f930c0cc59a9b72
Antonyxit <carlosmorga45@gmail.com>
2025-01-21 21:50:08 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,28 +1,23 @@
-
-
-const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/es.json`))
-  const tradutor = _translate.plugins.gc_tagall
-
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
   if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
+  global.dfail('admin', m, conn)
+  throw false
   }
-  const pesan = args.join` `;
-  const oi = `${global.ig} ${pesan}`;
-  let teks = `*${global.etiqueta}*\n${oi}\n\n*🌤️𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`;
-  for (const mem of participants) {
-    teks += `${global.emojietiqueta} @${mem.id.split('@')[0]}\n`;
+  let pesan = args.join` `
+  let oi = `_*BOT TUMBADO*_
+  
+  *🎭 Ventas tumbado Venta de bots, plataformas de streaming, cuentas ff, teams, clanes, diamantes.* ${pesan}`
+  let teks = `━━━━━━━━━━━━━━━━━━━━
+   ${oi}\n\n https://chat.whatsapp.com/In9Sjk9rGnUAE0oLnigkGB
+   ━━━━━━━━━━━━━━━━━━━━\n`
+  for (let mem of participants) {
+  teks += `⚜️ @${mem.id.split('@')[0]}\n`}
+  teks += `ㅤㅤㅤㅤ𝑻𝒖𝒎𝒃𝒂𝒅𝒐𝑽𝒆𝒏𝒕𝒂𝒔 🥷🏼`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
-  teks += `${global.packname}`;
-  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
-};
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
-handler.admin = true;
-handler.group = true;
-export default handler;
+  handler.help = ['tagall <mesaje>','invocar <mesaje>']
+  handler.tags = ['group']
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+  handler.admin = true
+  handler.group = true
+  export default handler
